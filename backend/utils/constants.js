bf68b5616a71d894743b90cd636bf21fea9a1230
Ashwinn-07 <ashwinkrishna785@gmail.com>
2025-04-17 07:18:21 +0530

    constants setup

---
+++ backend/utils/constants.js
@@ -0,0 +1,22 @@
+export const STATUS_CODES = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+  CONFLICT: 409,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+export const MESSAGES = {
+  SUCCESS: {
+    SURVEY_SUBMITTED: "Survey submitted successfully!",
+    LOGIN: "Admin logged in successfully",
+    ACCESS_DENIED: "Access denied. No token provided.",
+  },
+  ERROR: {
+    INVALID_TOKEN: "Invalid token.",
+    INVALID_CREDENTIALS: "Invalid credentials",
+  },
+};
