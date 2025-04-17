930903ccfd927206bfa2fb1072a579553eccb012
Ashwinn-07 <ashwinkrishna785@gmail.com>
2025-04-17 11:47:37 +0530

    properly configured zustand to persist the state

--- frontend/src/store/auth.js
+++ frontend/src/store/auth.js
@@ -1,8 +1,20 @@
 import { create } from "zustand";
+import { persist, createJSONStorage } from "zustand/middleware";
 
-export const useAuthStore = create((set) => ({
-  user: null,
-  isAuthenticated: false,
-  setAuth: (user) => set({ user, isAuthenticated: true }),
-  logout: () => set({ user: null, isAuthenticated: false }),
-}));
+export const useAuthStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      isAuthenticated: false,
+      setAuth: (user) => set({ user, isAuthenticated: true }),
+      logout: () => {
+        set({ user: null, isAuthenticated: false });
+        sessionStorage.removeItem("auth-storage");
+      },
+    }),
+    {
+      name: "auth-storage",
+      storage: createJSONStorage(() => sessionStorage),
+    }
+  )
+);
